662d8dce2a9975abffa9fcc34073bd819d7a5167
upslp <arturo15_13@hotmail.com>
2018-05-05 11:39:33 -0500

    Refactor para usar constante HelloWorld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld{
     public satic void main(String args[]){
-        System.out.println("hola");
+        final java.lang.String hola = "hola";
+        System.out.println(hola);
     }
 }
